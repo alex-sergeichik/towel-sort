a960b764e0ce062e64a31de4dcda5a76d07fe5c8
Alex Sergeichik <aleksandrsergeichik@gmail.com>
2021-02-25 18:48:37 +0300

    feat: add fnc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix == undefined || matrix.length == 0) {
+        let empty_arr = [];
+        return empty_arr;
+    }
+    else {
+        const mat = matrix.map((item, index) => index % 2 == 0 ? item : item.reverse());
+        return mat.reduce(function (a, b) {
+            return a.concat(b);
+        });
+    }
 }
+
